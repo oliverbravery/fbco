c82f55841d2e1e55800f3eac33b7b1ad5f74fa13
oliverbravery <dev@oliverbravery.uk>
2024-01-25 13:46:04 +0000

    Refactor code to load templates from files using single function.

--- main.py
+++ main.py
@@ -1,30 +1,39 @@
 from dotenv import load_dotenv
 import os
 
+# load environmental variables
 load_dotenv(override=True)
         
 from ChatOllamaFunctions import ChatOllamaFunctions
 from functions import weather, chat, search
-    
-def load_personality() -> str:
-    personality: str = ""
-    with open(os.getenv("PERSONALITY_FILE")) as f:
-        personality = f.read()
-    return personality
 
-def load_system_prompt_template() -> str:
+def load_from_template(path: str) -> str:
+    """
+    Load a template from a file and return it as a string.
+    Args:
+        path (str): The path to the template file.
+
+    Returns:
+        str: The template as a string.
+    """
     template: str = ""
-    with open(os.getenv("SYS_PROMPT_FILE")) as f:
+    with open(path) as f:
         template = f.read()
     return template
 
 MODEL = os.getenv("MODEL")
+
+# Load the templates from the files.
+personality_template: str = load_from_template(os.getenv("PERSONALITY_FILE"))
+sys_prompt_template: str = load_from_template(os.getenv("SYS_PROMPT_FILE"))
+
+# Create an instance on the ChatOllamaFunctions class.
 chat_llm: ChatOllamaFunctions = ChatOllamaFunctions(
     functions=[
         weather.WeatherFunction(), 
-        chat.ChatFunction(personality=load_personality()),
-        search.SearchFunction(model=MODEL, personality=load_personality())
+        chat.ChatFunction(personality=personality_template),
+        search.SearchFunction(model=MODEL, personality=personality_template)
         ], 
     model=MODEL,
-    prompt_template=load_system_prompt_template())
+    prompt_template=sys_prompt_template)
 
