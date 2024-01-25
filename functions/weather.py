67c40c8e24b51f983c1c4bf9e5fc79340d504016
oliverbravery <dev@oliverbravery.uk>
2024-01-25 14:42:30 +0000

    Added docstrings to WeatherFunction class

--- functions/weather.py
+++ functions/weather.py
@@ -1,11 +1,29 @@
 from Function import BaseFunction, Parameter, ParameterType, Property, PropertyType
 from pyowm import OWM
 from pyowm.weatherapi25.weather_manager import WeatherManager
+from pyowm.weatherapi25.weather import Weather
 import os
 
 class WeatherFunction(BaseFunction):
-    def __init__(self):
-        self.weather_manager: WeatherManager = OWM(os.getenv("OWM_API_KEY")).weather_manager()
+    """
+    A function derived from the BaseFunction class that allows the llm to get the current weather for a given location.
+
+    Args:
+        BaseFunction (_type_): The BaseFunction class to inherit from.
+    """
+    def __init__(self, owm_api_key: str = os.getenv("OWM_API_KEY")):
+        """
+        A function derived from the BaseFunction class that allows the llm to get the current weather for a given location.
+
+        Example json function call:
+        ```json
+        {"name": "get_current_weather", "arguments": {"location": "London,Uk", "units": "celsius"}}
+        ```
+
+        Args:
+            owm_api_key (str, optional): The OWM API key. Defaults to "OWM_API_KEY" environmental variable.
+        """
+        self.weather_manager: WeatherManager = OWM(owm_api_key).weather_manager()
         super().__init__(name="get_current_weather", 
                          description="Should only be used for checking the weather or temperature for a given location.", 
                          parameters=[
@@ -13,7 +31,7 @@ class WeatherFunction(BaseFunction):
                                         properties=[
                                              Property(name="location", 
                                                      type=PropertyType.STRING, 
-                                                     attribute={'description': 'The city and Country, e.g. Newcastle Upon Tyne,Uk'}),
+                                                     attribute={'description': 'The city and Country, e.g. London,Uk'}),
                                              Property(name="units",
                                                         type=PropertyType.STRING,
                                                         attribute={'enum': ["celsius", "fahrenheit"]})
@@ -22,6 +40,15 @@ class WeatherFunction(BaseFunction):
                          ])
         
     def __call__(self, arguments: dict) -> str:
-        weather = self.weather_manager.weather_at_place(arguments["location"]).weather
-        temp = weather.temperature(arguments["units"])["temp"]
+        """
+        Returns the current temperature for the given location in a formatted string.
+
+        Args:
+            arguments (dict): All the arguments for the function.
+
+        Returns:
+            str: The current temperature for the given location in a formatted string.
+        """
+        weather: Weather = self.weather_manager.weather_at_place(arguments["location"]).weather
+        temp: str = weather.temperature(arguments["units"])["temp"]
         return f'The temperature in {arguments["location"]} is {temp} degrees {arguments["units"]}.'
