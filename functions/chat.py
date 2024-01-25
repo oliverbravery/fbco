6d6a93200abfeacc87a974864bd696b0e5b82e86
oliverbravery <dev@oliverbravery.uk>
2024-01-25 14:26:47 +0000

    Added docstrings to ChatFunction

--- functions/chat.py
+++ functions/chat.py
@@ -1,7 +1,24 @@
 from Function import BaseFunction, Parameter, ParameterType, Property, PropertyType
 
 class ChatFunction(BaseFunction):
+    """
+    A function derived from the BaseFunction class that allows the llm to respond to the user in a chat-like manner.
+
+    Args:
+        BaseFunction (_type_): The BaseFunction class to inherit from.
+    """
     def __init__(self, personality: str = ""):
+        """
+        Instantiates a new ChatFunction object with the given personality.
+        
+        Example json function call:
+        ```json
+        {"name": "response_normal", "arguments": {"output": "Hello, how are you?"}}
+        ```
+
+        Args:
+            personality (str, optional): The personality of the llm. Defaults to "".
+        """
         super().__init__(name="response_normal", 
                          description="Respond to the user normally. If no other function is called, this function will be called.", 
                          parameters=[
@@ -15,4 +32,13 @@ class ChatFunction(BaseFunction):
                          ])
         
     def __call__(self, arguments: dict) -> str:
+        """
+        Returns the output from the arguments.
+
+        Args:
+            arguments (dict): All the arguments for the function.
+
+        Returns:
+            str: The output from the arguments.
+        """
         return str(arguments["output"])
